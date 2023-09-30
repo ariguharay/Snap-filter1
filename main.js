a6ca6ae3177ad03df3f71a5ee4f8e67abcb0e00f
ariguharay <118127149+ariguharay@users.noreply.github.com>
2023-09-30 15:17:23 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(350, 300);
+    canvas.position(480,250);
+}
+
+function draw() {
+}
+
+function take_snapshot(){
+    save('myFilterImage.png')
+}
